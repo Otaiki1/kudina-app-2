bffd6d40798bed13370552bc7b2d63243e50fea0
Otaiki1 <otaikisadiq2804@gmail.com>
2022-10-26 12:36:32 +0100

    fix: image not displaying in topbar

--- components/Topbar.js
+++ components/Topbar.js
@@ -1,11 +1,11 @@
 import avatar from "../assets/user_avatar.jpg"
-
+import Image from "next/image";
 export default function Topbar(){
 
     return(
         <div className="d-flex justify-content-between pt-4">
             <div className="d-flex w-50 justify-content-around">
-                <img src={avatar} alt="" className="rounded-circle img-thumbnail img-responsive w-25" />
+                <Image src={avatar} alt="" className="rounded-circle img-thumbnail img-responsive w-25" />
                 <div className="d-flex flex-column">
                     <p className="text-secondary mb-0">Ofafa Agent</p>
                     <p>Halima</p>
