2888ba53cc3f326c14db2cc18993b170d42fb9c2
Otaiki1 <otaikisadiq2804@gmail.com>
2022-10-26 14:27:10 +0100

    refac: setup logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import Link from "next/link"
+import Image from "next/image";
+import logo from "../assets/Frame.png"
+
 
 export default function SignUp(){
 const [filledForm, setFilledForm] = useState(false);
@@ -17,8 +20,9 @@ useEffect(() => {
 
 
     return(
-        <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+        <div className="w-100 h-100 d-flex justify-content-center align-items-center bg-success">
             <div className="container">
+            <Image className="img-responsive d-block mx-auto" alt="logo" src={logo}/>
             <div>
                 <label htmlFor="email"></label>
                 <input typeof="email" name="email" placeholder="Email Address" className="form-control" onChange={(e) => setEmail(e.target.value)}/>
